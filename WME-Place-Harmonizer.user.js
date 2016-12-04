890bfb5ed7ad143e1505e0f38869326fc8b5624e
t0cableguy <t0cableguy@gmail.com>
2016-12-04 16:22:46 -0500

    Update WME-Place-Harmonizer.user.js

--- WME-Place-Harmonizer.user.js
+++ WME-Place-Harmonizer.user.js
@@ -13,8 +13,8 @@
 // @version         1.1.35
 // @description     Harmonizes, formats, and locks a selected place
 // @author          WMEPH development group
-// @include         https://.*\.waze\.com\/(.*\/)?editor\/.*/i
-// @exclude	    https://.*\.waze\.com\/user\/.*/i
+// @include         /.*\.waze\.com\/(.*\/)?editor\/.*/i
+// @exclude	    /.*\.waze\.com\/user\/.*/i
 // @grant	   none
 // @require https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 
