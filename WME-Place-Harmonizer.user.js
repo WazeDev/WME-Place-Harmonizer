a36d983931e8dbc59753f02e0e7165ba3cafe4bc
mapomatic <mapomatic.waze@gmail.com>
2017-01-14 21:27:26 -0500

    Fix for incorrect area / point warnings when multiple categories are present.

--- WME-Place-Harmonizer.user.js
+++ WME-Place-Harmonizer.user.js
@@ -13,7 +13,7 @@
 // ==UserScript==
 // @name        WME Place Harmonizer Beta
 // @namespace   https://github.com/WazeUSA/WME-Place-Harmonizer/raw/master/WME-Place-Harmonizer.user.js
-// @version     1.1.79-pt-vs-area.3
+// @version     1.1.79
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH development group
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
