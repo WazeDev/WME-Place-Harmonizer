b118e3eb6273949181c16bf79b3c6f30ca6c15c9
mapomatic <mapomatic.waze@gmail.com>
2017-02-03 23:05:51 -0500

    Removed R2+ restriction for using script.

--- WME-Place-Harmonizer.user.js
+++ WME-Place-Harmonizer.user.js
@@ -13,7 +13,7 @@
 // ==UserScript==
 // @name        WME Place Harmonizer (GM)
 // @namespace   https://github.com/WazeUSA/WME-Place-Harmonizer/raw/master/WME-Place-Harmonizer.user.js
-// @version     1.2.0
+// @version     1.2.1
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH development group
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
@@ -2228,6 +2228,8 @@ body .ui-tooltip {
     function runPH() {
         // Script update info
         var WMEPHWhatsNewList = [  // New in this version
+            '1.2.1: Removed R2+ restriction for using this script.',
+            '1.2.0: Production release.',
             '1.1.97: Added regex place name matching for increased flexibility.',
             '1.1.96: Changed "City Missing." to "No city" to be consistent with other flag messages.',
             '1.1.96: Hospital / gas station and PLA "special" highlights only display if no lock (L1).',
@@ -2267,6 +2269,7 @@ body .ui-tooltip {
             '1.1.68: Added "Missing External Provider" and option to treat as non-critical.'
         ];
         var WMEPHWhatsNewMetaList = [  // New in this major version
+            'WMEPH is now available for R1 editors to use!',
             'Yellow "caution" map highlights.',
             'Missing external provider (Google linked place) is flagged if R3+.',
             'Optional setting to treat missing external provider link as a blue flag instead of red.',
@@ -2842,11 +2845,6 @@ body .ui-tooltip {
 
         // Only run the harmonization if a venue is selected
         function harmonizePlace() {
-            // Script is only for R2+ editors
-            if (!betaUser && usrRank < 2) {
-                alert("Script is currently available for editors of Rank 2 and up.");
-                return;
-            }
             // Beta version for approved users only
             if (isDevVersion && !betaUser) {
                 alert("Please sign up to beta-test this script version.\nSend a PM or Slack-DM to t0cableguy or Tonestertm, or post in the WMEPH forum thread. Thanks.");
