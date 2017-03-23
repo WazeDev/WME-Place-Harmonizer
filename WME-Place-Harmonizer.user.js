778d4d630a282d7682cc00597a3b7205067df183
t0cableguy <t0cableguy@users.noreply.github.com>
2017-03-23 00:41:43 -0400

    Update WME-Place-Harmonizer.user.js

--- WME-Place-Harmonizer.user.js
+++ WME-Place-Harmonizer.user.js
@@ -12,7 +12,7 @@
 
 // ==UserScript==
 // @name        WME Place Harmonizer Beta
-// @namespace   https://github.com/WazeUSA/WME-Place-Harmonizer/raw/master/WME-Place-Harmonizer.user.js
+// @namespace   https://github.com/WazeUSA/WME-Place-Harmonizer/raw/Beta/WME-Place-Harmonizer.user.js
 // @version     1.2.5
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
