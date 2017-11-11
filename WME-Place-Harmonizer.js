c259f62a4f8e2e085da52339bb5a761746a744f2
mapomatic <mapomatic.waze@gmail.com>
2017-11-11 09:51:45 -0500

    Changed gas stations to match as long as brand is in name. Minor update to hours parser to use native JS rather than some functions built in to WME code.

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -11,7 +11,7 @@
 /* global Node */
 
 // ==UserScript==
-// @name        WME Place Harmonizer
+// @name        WME Place Harmonizer Beta
 // @namespace   WazeUSA
 // @version     1.3.41
 // @description Harmonizes, formats, and locks a selected place
@@ -3423,7 +3423,7 @@
                             bannButt.gasMkPrim.active = true;
                             lockOK = false;
                         } else {
-                            newName = PNHMatchData[ph_name_ix];
+                            //newName = PNHMatchData[ph_name_ix];
                         }
                     } else if (updatePNHName) {  // if not a special category then update the name
                         newName = PNHMatchData[ph_name_ix];
@@ -5477,9 +5477,12 @@
                 phlogdev('No hours');
                 return false;
             }
-            var today = Date.today();
+
+            var today = new Date();
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
             inputHoursParse = inputHoursParse.replace(/\btoday\b/g, today.toLocaleDateString(I18n.locale, {weekday:'short'}).toLowerCase());
-            inputHoursParse = inputHoursParse.replace(/\btomorrow\b/g, today.addDays(1).toLocaleDateString(I18n.locale, {weekday:'short'}).toLowerCase());
+            inputHoursParse = inputHoursParse.replace(/\btomorrow\b/g, tomorrow.toLocaleDateString(I18n.locale, {weekday:'short'}).toLowerCase());
             inputHoursParse = inputHoursParse.replace(/\u2013|\u2014/g, "-");  // long dash replacing
             inputHoursParse = inputHoursParse.replace(/[^a-z0-9\:\-\. ~]/g, ' ');  // replace unnecessary characters with spaces
             inputHoursParse = inputHoursParse.replace(/\:{2,}/g, ':');  // remove extra colons
@@ -7217,7 +7220,7 @@
                         var re = new RegExp(match[1].replace(/\\/,'\\'),'i');
                         PNHStringMatch = re.test(item.attributes.name);
                     }
-                } else if (specCases.indexOf('strMatchAny') > -1 || currMatchData[ph_category1_ix] === 'Hotel') {  // Match any part of WME name with either the PNH name or any spaced names
+                } else if (specCases.indexOf('strMatchAny') > -1 || currMatchData[ph_category1_ix] === 'Hotel' || currMatchData[ph_category1_ix] === 'Gas Station') {  // Match any part of WME name with either the PNH name or any spaced names
                     allowMultiMatch = true;
                     var spaceMatchList = [];
                     spaceMatchList.push( currMatchData[ph_name_ix].toUpperCase().replace(/ AND /g, ' ').replace(/^THE /g, '').replace(/[^A-Z0-9 ]/g, ' ').replace(/ {2,}/g, ' ') );
