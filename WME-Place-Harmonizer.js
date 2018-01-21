95f9bf31b0abfb7bd2be8630ebc3b4ea18137558
mapomatic <mapomatic.waze@gmail.com>
2018-01-20 22:18:48 -0500

    remove Beta from name

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -11,7 +11,7 @@
 /* global Node */
 
 // ==UserScript==
-// @name        WME Place Harmonizer Beta
+// @name        WME Place Harmonizer
 // @namespace   WazeUSA
 // @version     1.3.66
 // @description Harmonizes, formats, and locks a selected place
