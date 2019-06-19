54068aacd79909fcdf257720b8d9fc5c24b2d42c
mapomatic <mapomatic.waze@gmail.com>
2019-06-19 15:51:16 -0400

    version bump

--- WME-Place-Harmonizer.user.js
+++ WME-Place-Harmonizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WME Place Harmonizer Beta (pnh-update)
 // @namespace   WazeUSA
-// @version     2019.06.19.001
+// @version     2019.06.19.002
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
