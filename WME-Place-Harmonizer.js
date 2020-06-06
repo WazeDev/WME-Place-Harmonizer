db264992dcba4577cc6f80c32a9a7a2d6df07534
tonestertm <tonewaze@gmail.com>
2020-06-06 14:44:50 -0700

    Update WME-Place-Harmonizer.js

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        WME Place Harmonizer Beta
 // @namespace   WazeUSA
-// @version     2020.06.03.008
+// @version     2020.06.06.001
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -4358,7 +4358,7 @@ function harmonizePlaceGo(item, useFlag, actions) {
                     /* eslint-disable no-cond-assign */
 
                     // find any button/message flags in the special case (format: buttOn_xyzXyz, etc.)
-                    if (match == specCase.match(/^buttOn_(.+)/i)) {
+                    if (match = specCase.match(/^buttOn_(.+)/i)) {
                         [, scFlag] = match;
                         let flag = null;
                         switch (scFlag) {
@@ -4384,32 +4384,32 @@ function harmonizePlaceGo(item, useFlag, actions) {
                                 console.error('WMEPH:', `Could not process specCase value: buttOn_${scFlag}`);
                         }
                         _buttonBanner[scFlag] = flag;
-                    } else if (match == specCase.match(/^buttOff_(.+)/i)) {
+                    } else if (match = specCase.match(/^buttOff_(.+)/i)) {
                         [, scFlag] = match;
                         _buttonBanner[scFlag] = null;
-                    } else if (match == specCase.match(/^messOn_(.+)/i)) {
+                    } else if (match = specCase.match(/^messOn_(.+)/i)) {
                         [, scFlag] = match;
                         _buttonBanner[scFlag].active = true;
-                    } else if (match == specCase.match(/^messOff_(.+)/i)) {
+                    } else if (match = specCase.match(/^messOff_(.+)/i)) {
                         [, scFlag] = match;
                         _buttonBanner[scFlag].active = false;
-                    } else if (match == specCase.match(/^psOn_(.+)/i)) {
+                    } else if (match = specCase.match(/^psOn_(.+)/i)) {
                         [, scFlag] = match;
                         _servicesBanner[scFlag].actionOn(actions);
                         _servicesBanner[scFlag].pnhOverride = true;
-                    } else if (match == specCase.match(/^psOff_(.+)/i)) {
+                    } else if (match = specCase.match(/^psOff_(.+)/i)) {
                         [, scFlag] = match;
                         _servicesBanner[scFlag].actionOff(actions);
                         _servicesBanner[scFlag].pnhOverride = true;
                     }
 
                     // If brand is going to be forced, use that.  Otherwise, use existing brand.
-                    if (match == /forceBrand<>([^,<]+)/i.exec(pnhMatchData[phSpecCaseIdx])) {
+                    if (match = /forceBrand<>([^,<]+)/i.exec(pnhMatchData[phSpecCaseIdx])) {
                         [, newBrand] = match;
                     }
 
                     // parseout localURL data if exists (meaning place can have a URL distinct from the chain URL
-                    if (match == specCase.match(/^localURL_(.+)/i)) {
+                    if (match = specCase.match(/^localURL_(.+)/i)) {
                         [, localURLcheck] = match;
                     }
 
@@ -4420,7 +4420,7 @@ function harmonizePlaceGo(item, useFlag, actions) {
                     }
 
                     // Gas Station forceBranding
-                    if (['GAS_STATION'].includes(priPNHPlaceCat) && (match == specCase.match(/^forceBrand<>(.+)/i))) {
+                    if (['GAS_STATION'].includes(priPNHPlaceCat) && (match = specCase.match(/^forceBrand<>(.+)/i))) {
                         const [, forceBrand] = match;
                         if (item.attributes.brand !== forceBrand) {
                             actions.push(new UpdateObject(item, { brand: forceBrand }));
@@ -4538,13 +4538,13 @@ function harmonizePlaceGo(item, useFlag, actions) {
                         } else if (tempLocalURL[tlix] === 'ph_longitudePM') {
                             _customStoreFinderLocalURL += itemGPS.lon;
                         } else if (tempLocalURL[tlix] === 'ph_latitudePMBuffMin') {
-                            _customStoreFinderLocalURL += (itemGPS.lat - 0.15).toString();
+                            _customStoreFinderLocalURL += (itemGPS.lat - 0.025).toString();
                         } else if (tempLocalURL[tlix] === 'ph_longitudePMBuffMin') {
-                            _customStoreFinderLocalURL += (itemGPS.lon - 0.15).toString();
+                            _customStoreFinderLocalURL += (itemGPS.lon - 0.025).toString();
                         } else if (tempLocalURL[tlix] === 'ph_latitudePMBuffMax') {
-                            _customStoreFinderLocalURL += (itemGPS.lat + 0.15).toString();
+                            _customStoreFinderLocalURL += (itemGPS.lat + 0.025).toString();
                         } else if (tempLocalURL[tlix] === 'ph_longitudePMBuffMax') {
-                            _customStoreFinderLocalURL += (itemGPS.lon + 0.15).toString();
+                            _customStoreFinderLocalURL += (itemGPS.lon + 0.025).toString();
                         } else if (tempLocalURL[tlix] === 'ph_houseNumber') {
                             _customStoreFinderLocalURL += (item.attributes.houseNumber ? item.attributes.houseNumber : '');
                         } else {
