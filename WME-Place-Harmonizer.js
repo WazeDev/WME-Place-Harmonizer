0aa617a4c89601dfa5507361ddadd0082f481971
MapOMatic <mapomatic.waze@gmail.com>
2021-04-22 19:43:45 -0400

    update USPS url

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        WME Place Harmonizer Beta
 // @namespace   WazeUSA
-// @version     2020.12.10.001
+// @version     2021.04.22.001
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -5335,7 +5335,7 @@ function harmonizePlaceGo(item, useFlag, actions) {
                 _buttonBanner.isThisAPostOffice = Flag.IsThisAPostOffice.eval(item, _newName).flag;
             } else {
                 if (hpMode.harmFlag) {
-                    _customStoreFinderURL = 'https://tools.usps.com/go/POLocatorAction.action';
+                    _customStoreFinderURL = 'https://tools.usps.com/find-location.htm';
                     _customStoreFinder = true;
                     _buttonBanner.PlaceWebsite = new Flag.PlaceWebsite();
                     _buttonBanner.NewPlaceSubmit = null;
