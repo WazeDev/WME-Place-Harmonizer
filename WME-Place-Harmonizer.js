090859e6d932efb5c554c7ef310cc14d6f3f267c
David Nelson <1472423+DaJoNel@users.noreply.github.com>
2023-08-24 12:47:45 -0500

    Update to use @match statements
    Use @match statements so that the script loads in Tampermonkey.

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -4,7 +4,11 @@
 // @version     2023.08.22.001
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
-// @include     /^https:\/\/www\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @match       https://www.waze.com/editor*
+// @match       https://www.waze.com/*/editor*
+// @match       https://beta.waze.com/editor*
+// @match       https://beta.waze.com/*/editor*
+// @exclude     https://www.waze.com/user/editor*
 // @require     https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @require     https://greasyfork.org/scripts/37486-wme-utils-hoursparser/code/WME%20Utils%20-%20HoursParser.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js
