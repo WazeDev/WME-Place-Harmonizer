584e9c692fb8746882a2ea521c07985ee04a4d27
MapOMatic <mapomatic.waze@gmail.com>
2023-09-14 12:49:41 -0400

    bump version and update include statement to work in prod and beta WME

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        WME Place Harmonizer Beta
 // @namespace   WazeUSA
-// @version     2023.09.01.001
+// @version     2023.09.14.001
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
-// @include     /^https:\/\/beta\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
 // @require     https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @require     https://greasyfork.org/scripts/37486-wme-utils-hoursparser/code/WME%20Utils%20-%20HoursParser.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js
