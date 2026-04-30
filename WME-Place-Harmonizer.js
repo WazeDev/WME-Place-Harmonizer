397297ad8a6d2336cc95f9cf211cbbe051c92e9e
JS55CT <js55ct.waze.ct@gmail.com>
2026-04-30 13:22:26 -0400

    Commit current work before resolving PR conflicts

--- WME-Place-Harmonizer.js
+++ WME-Place-Harmonizer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        WME Place Harmonizer
+// @name        WME Place Harmonizer (Beta)
 // @namespace   WazeUSA
-// @version     2026.04.29.000
+// @version     2026.04.30.000
 // @description Harmonizes, formats, and locks a selected place
 // @author      WMEPH Development Group
 // @include      https://www.waze.com/editor*
